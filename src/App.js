8fcdc2588fec186ec090b3e1deddffea308b09ce
xiushak <30637265+xiushak@users.noreply.github.com>
2023-11-19 21:44:50 -0500

    inital folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="text-center w-100">WELCOME TO POKEMART</div>;
+  return <div className="text-end w-100">WELCOME TO POKEMART</div>;
 }
 
 export default App;

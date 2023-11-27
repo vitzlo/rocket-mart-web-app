fe81166993d5ce5d9739e1ad3b36fe15d913ab75
xiushak <30637265+xiushak@users.noreply.github.com>
2023-11-27 16:17:30 -0500

    loading/no result page and messing around

--- src/SearchResults/index.js
+++ src/SearchResults/index.js
@@ -5,21 +5,45 @@ import SmallPokemon from "../Pokemon/SmallView";
 
 function SearchResults() {
   const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { search } = useParams();
 
   useEffect(() => {
+    setLoading(true);
+    setSearchResults([]);
     searchPokemonBySubstring(search).then((results) => {
       setSearchResults(results);
+      setLoading(false);
     });
   }, [search]);
   return (
     <div className="container-fluid">
       <div className="row">
-        {searchResults.map((pokemon, index) => (
-          <div className="col-auto">
-            <SmallPokemon key={index} pokemonName={pokemon} />
+        {searchResults.length > 0 ? (
+          searchResults.map((pokemon, index) => (
+            <div className="col-auto">
+              <SmallPokemon key={index} pokemonName={pokemon} />
+            </div>
+          ))
+        ) : loading ? (
+          <div className="text-center">
+            <h1>Loading...</h1>
+            <img
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FS0dlRVeqjpoTC%2Fgiphy.gif&f=1&nofb=1&ipt=1fcd67f50a18eb323cb536b60a98d4543333c563cdea04fae3c1a499bbb24f4e&ipo=images"
+              alt="loading"
+              className="w-50"
+            />
           </div>
-        ))}
+        ) : (
+          <div className="text-center">
+            <h1>Sorry... theres no pokemon named {search.replace("-", " ")}</h1>
+            <img
+              src="https://i0.wp.com/media1.giphy.com/media/dJYoOVAWf2QkU/giphy.gif?resize=640%2C640&ssl=1&crop=1"
+              alt="no results"
+              className="w-50"
+            />
+          </div>
+        )}
       </div>
     </div>
   );

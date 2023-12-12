0d6f7fbdebbce192d5a9a63e02c2a0ddfbd11cb7
Victor Louie <58094419+vitzlo@users.noreply.github.com>
2023-12-11 23:28:45 -0500

    Local list of types

--- src/Home/types.js
+++ src/Home/types.js
@@ -1,27 +1,34 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import Badge from "../Pokemon/LargeView/Types/pokemonTypeBadge";
 
 const Types = () => {
-  const API_BASE = "https://pokeapi.co/api/v2";
-  // state variable for types probably not needed, could probably make a constant for all the types
-  const [types, setTypes] = useState([]);
+  const types = [
+    "normal",
+    "fighting",
+    "flying",
+    "poison",
+    "ground",
+    "rock",
+    "bug",
+    "ghost",
+    "steel",
+    "fire",
+    "water",
+    "grass",
+    "electric",
+    "psychic",
+    "ice",
+    "dragon",
+    "dark",
+    "fairy",
+  ];
 
-  const fetchTypes = async () => {
-    const response = await axios.get(`${API_BASE}/type`);
-    const prunedTypes = response.data.results.filter((t) => t.name !== "unknown" && t.name !== "shadow");
-    setTypes(prunedTypes);
-  };
-
-  useEffect(() => {
-    fetchTypes();
-  }, []);
   return (
     <div className="text-center m-2">
       <h2>Search by Types</h2>
       <div className="row mx-4">
         {types.map((type) => (
-          <Badge key={type.name} type={type.name} />
+          <Badge key={type} type={type} />
         ))}
       </div>
     </div>
